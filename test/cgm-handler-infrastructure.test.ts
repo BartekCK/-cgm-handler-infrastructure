6bcaa753f5af686f349650f7cde4769c83a5ee08
Bartek <>
2023-01-25 19:17:07 +0100

    Initial commit

---
+++ test/cgm-handler-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CgmHandlerInfrastructure from '../lib/cgm-handler-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cgm-handler-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CgmHandlerInfrastructure.CgmHandlerInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
